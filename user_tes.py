a1b86a404ac1d47984429f4862d41b73e4630598
eude313 <eudes.charles@student.moringaschool.com>
2021-08-05 18:56:41 +0300

    add changes

--- user_tes.py
+++ user_tes.py
@@ -5,6 +5,10 @@ from user import User
 class TestUser(unittest.TestCase):
     def setUp(self):
         
-        self.new_user = User("James","Muriuki","0712345678","james@ms.com")# create user object
+        self.new_user = User("carbon","Miki","0712345678","miki@ms.com")
 
-
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name, "carbon")
+        self.assertEqual(self.new_user.last_name, "Miki")
+        self.assertEqual(self.new_user.phone_number, "0712345678")
+        self.assertEqual(self.new_user.email, "miki@ms.com") 
