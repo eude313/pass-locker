878215653a4359f7bfd7fd127baaee04b66e8603
eude313 <eudes.charles@student.moringaschool.com>
2021-08-05 15:55:05 +0300

    add save method

--- info.py
+++ info.py
@@ -1,8 +1,12 @@
 class info:
     #class that generates new credential for users 
     pass
-    info_array = []
+    infomation_array = []
     def __init__(self,user_name,password,email):
         self.user_name = user_name
         self.email = email
         self.password = password
+        
+ #save_contact method saves info       
+    def save_info(self):    
+        info.infomation_array.append(self) 
