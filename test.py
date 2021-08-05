f86ec451b81d164a81bc40f0900e59f607270452
eude313 <eudes.charles@student.moringaschool.com>
2021-08-05 14:54:03 +0300

    add initial commit

--- test.py
+++
@@ -1,7 +0,0 @@
-from user import first
-import unittest
-
-#first test
-class TestContact(unittest.TestCase):
-    def setUp(self): 
-        self.name = first('carbon')
--- user.py
+++ user.py
@@ -1,3 +1,20 @@
-class first:
-    def __init__(self, name):
-        self.name = name 
+import pyperclip
+
+class User:
+    
+    pass
+
+    user_list = []
+    
+    def __init__(self,first_name,last_name,phone_number,email):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+    user_list = [] #this is a usser list
+    
+    #saving user information into usser list
+    def save_user(self):
+        User.user_list.append(self)
+        
