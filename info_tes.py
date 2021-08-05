b6a03ee8172bbb7f1b9a84b33333399994cccd30
eude313 <eudes.charles@student.moringaschool.com>
2021-08-05 16:40:14 +0300

    add initial commit

--- info_tes.py
+++ info_tes.py
@@ -4,3 +4,10 @@ from info import info
 class TestUSer(unittest.TestCase):
     def setUp(self):
       self.new_credential = info("user_name","password","email@ms.com")  
+    #tearDown method should
+    def tearDown(self):
+        info.credential_array = []
+    def test_init(self):
+        self.assertEqual(self.new_credential.user_name, "user_name")
+        self.assertEqual(self.new_credential.email, "email@ms.com")
+        self.assertEqual(self.new_credential.password, "password")
