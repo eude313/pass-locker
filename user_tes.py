060d6a5447d40714af0ca5868c1c8ec286617cba
eude313 <eudes.charles@student.moringaschool.com>
2021-08-05 18:58:07 +0300

    add changes

--- user_tes.py
+++ user_tes.py
@@ -12,3 +12,11 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name, "Miki")
         self.assertEqual(self.new_user.phone_number, "0712345678")
         self.assertEqual(self.new_user.email, "miki@ms.com") 
+    def test_save_user(self):
+        self.new_user.save_user()#saving the new user
+        self.assertEqual(len(User.user_list),1)
+    def test_save_multiple_user(self):
+        self.new_user.save_user()
+        test_user = User("Test","user","0712345678","test@user.com")#save user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
